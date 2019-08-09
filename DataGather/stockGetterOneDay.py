ce426fa1072b025dabf07f8d69d460eb6698361b
Anderson Molter <andersonmolter1@gmail.com>
2019-08-08 22:03:20 -0400

    Added Basic Data Gather

---
+++ DataGather/stockGetterOneDay.py
@@ -0,0 +1,30 @@
+import pandas as pd
+pd.set_option('display.max_rows', None)
+pd.set_option('expand_frame_repr', False)
+import time
+import datetime
+from datetime import date
+import pandas_datareader.data as web
+symbol = []
+with open('/home/anderson/stockNames.txt') as f:  
+    for line in f:
+        symbol.append(line.strip())
+f.close
+
+start = datetime.date.today()
+end = datetime.date.today()
+
+i=0
+while i<len(symbol):
+    try:
+        f = web.DataReader(symbol[i], 'yahoo', start, end)
+        f.insert(0,'Symbol',symbol[i])
+        f = f.drop(['Adj Close'], axis=1)
+        print (f)
+    except :
+        print("No information for ticker:" )
+        print (symbol[i])
+        continue
+    i=i+1
+
+
