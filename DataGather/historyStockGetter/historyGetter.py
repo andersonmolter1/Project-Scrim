12bde73bc935253ae525edb8e4732157aae099e5
andersonmolter1 <andersonmolter1@gmail.com>
2019-08-12 22:07:07 -0400

    Updated scripts to take in entire companies history data

--- DataGather/historyStockGetter/historyGetter.py
+++ DataGather/historyStockGetter/historyGetter.py
@@ -18,23 +18,26 @@ def gettickername():
 
 
 def getstockdata(reload_sp500=False, symbol=symbol):
-
+    i = 1950
     if not os.path.exists('stockData_dfs'):
         os.makedirs('stockData_dfs')
-    start = dt.datetime(2000, 1, 1)
+    start = dt.datetime(i, 1, 1)
     end = dt.datetime.today()
-    i = 2000
     for symbol in symbol:
-        if not os.path.exists('stockData_dfs/{}.csv'.format(symbol)):
-            try:
-                df = web.DataReader(symbol, 'yahoo', start, end)
-                df.to_csv('stockData_dfs/{}.csv'.format(symbol))
-            except:
-                print(symbol)
-                continue
-        else:
-            print('Already have {}'.format(symbol))
-
-
+        print(symbol)
+        i = 1950
+        while i < 2019:
+            start = dt.datetime(i, 1, 1)
+            i = i + 1
+            if not os.path.exists('stockData_dfs/{}.csv'.format(symbol)):
+                try:
+                    df = web.DataReader(symbol, 'yahoo', start, end)
+                    df.to_csv('stockData_dfs/{}.csv'.format(symbol))
+                    df = pd.read_csv(symbol, parse_dates=True, index_col=0)
+                    df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+                    print(df.head())
+                except:
+                    break
+                    break
 gettickername()
 getstockdata()
